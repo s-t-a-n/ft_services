0fb82c6a80d9f596c0cce1357a539aec2f3c3bd6
Stan Verschuuren <sverschu@student.codam.nl>
2020-07-22 22:14:04 +0200

    fixed ssh for vsftpd

--- srcs/6-vsftpd/docker-srcs/run.sh
+++ srcs/6-vsftpd/docker-srcs/run.sh
@@ -26,16 +26,16 @@ if ! grep $CLUSTER_ADMIN $VSFTPD_USERLIST 2>/dev/null 1>&2; then
 	echo $CLUSTER_ADMIN >> $VSFTPD_USERLIST || exit 1
 fi
 
-# if [ ! -f /etc/ssl/private/vsftpd.pem ]; then
-# 	openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout /etc/ssl/private/vsftpd.pem -subj "/C=NL/ST=Test/L=Test/O=Test/CN=vsftpd" -out /etc/ssl/private/vsftpd.pem || exit 1
-# fi
+if ! -d /home/$CLUSTER_ADMIN; then
+	mkdir -p /home/$CLUSTER_ADMIN/.ssh || exit  1
+	chmod 700 /home/$CLUSTER_ADMIN/.ssh/ || exit 1
+	echo "$CLUSTER_ADMIN_SSHPUB" > /home/$CLUSTER_ADMIN/.ssh/authorized_keys || exit 1
+	chmod 500 /home/$CLUSTER_ADMIN/.ssh/authorized_keys || exit 1
+	chown -R $CLUSTER_ADMIN:$CLUSTER_ADMIN /home/$CLUSTER_ADMIN/ || exit 1
+fi
 
-mkdir -p /home/$CLUSTER_ADMIN/.ssh && chmod 700 /home/$CLUSTER_ADMIN/.ssh/ && echo "$CLUSTER_ADMIN_SSHPUB" > /home/$CLUSTER_ADMIN/.ssh/authorized_keys && chmod 500 /home/$CLUSTER_ADMIN/.ssh/authorized_keys || exit 1
+rm -f /etc/motd
 
 supervisord -c /etc/supervisord/supervisord.conf
 
 exit $?
-
-#/usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
-
-#tail -f /dev/null # remove when ready
--- srcs/7-influxdb/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-basedir=$(dirname "$0")
-eval $(minikube docker-env)
-
-kubectl apply -f $basedir/tmp_build-env-configmap.yaml      \
-&& docker build -t vsftpd-alpine:v1 $basedir                \
-&& kubectl apply -f $basedir/tmp_alpine-vsftpd.yaml
-
-exit $?
