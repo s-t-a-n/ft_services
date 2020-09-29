3f58e28d82fcacd791d214ad5119368bcc908391
Stan Verschuuren <sverschu@student.codam.nl>
2020-09-29 22:49:04 +0200

    changed ordering of servicedeployments, started on phpfpm, etc.

---
+++ srcs/5-phpfpm/docker-srcs/run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+mkdir -p /run/nginx && mkdir -p /var/log/nginx && mkdir -p /data/http #|| exit 1
+
+# uncomment to load a custom default.conf
+#if [ ! -d /data/http/default ] ||  [ ! -f /data/http/default/index.html ]; then
+#	mkdir -p /data/http/default	\
+#		&& echo "Hello World" > /data/http/default/index.html \
+#		&& chown -R nginx:nginx /data/http #|| exit 1
+#fi
+
+if [ -d /data/nginx-conf ] && [ ! -L /etc/nginx ]; then 
+	echo "Nginx configuration already exists in /data/nginx..."
+	BACKUPD="/data/nginx-$(date +%D)"
+	echo "Moving existing configuration to $BACKUPD"
+	mv /data/nginx-conf $BACKUPD #|| exit 1
+fi
+
+if [ ! -L /etc/nginx ]; then
+	mv /etc/nginx /data/nginx-conf && ln -s /data/nginx-conf /etc/nginx
+fi
+
+if [ ! -L /data/nginx-conf/sites-enabled/default ]; then
+	(cd /data/nginx-conf/sites-enabled/ && ln -s ../sites-available/default.conf ./) #|| exit 1
+fi
+
+rm -f /etc/motd
+supervisord -c /etc/supervisord/supervisord.conf
+
+tail -f /dev/null # remove when ready
+
+exit $?
