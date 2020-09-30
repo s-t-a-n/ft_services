884d7daa115b5eb5788b3e1776f721bbfbe25327
s-t-a-n <stan@wopwop.cc>
2020-09-30 17:46:42 +0200

    added php(fpm) versioning, removed undefined volumeclaimes from alpine-phpfpm.yaml

--- srcs/5-phpfpm/setup.sh
+++ srcs/5-phpfpm/setup.sh
@@ -2,13 +2,16 @@
 basedir=$(dirname "$0")
 source $basedir/../global_scripts/logp.sh
 source $basedir/../global_scripts/wait_for_pod.sh
+source $basedir/config
 eval $(minikube docker-env)
 
 trap "rm -rf $basedir/docker-srcs/global_container_scripts && rm -rf $basedir/docker-srcs/global_container_confs" EXIT INT
 
 cp -r $basedir/../global_container_scripts $basedir/docker-srcs/			\
 && cp -r $basedir/../global_container_confs $basedir/docker-srcs/			\
-&& docker build -f $basedir/Dockerfile -t phpfpm-alpine:v1 $basedir			\
+&& docker build --build-arg PHP_VERSION=$PHP_VERSION 						\
+				--build-arg PHPFPM_VERSION=$PHPFPM_VERSION					\
+				-f $basedir/Dockerfile										\
+				-t phpfpm-alpine:v1 $basedir								\
 && kubectl apply -f $basedir/alpine-phpfpm.yaml
-
 exit $?
