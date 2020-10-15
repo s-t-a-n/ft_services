f6bdb69f07e04c53d929c5e74d5e60cc7e51e968
Stan Verschuuren <sverschu@student.codam.nl>
2020-10-15 22:45:30 +0200

    temporarily disabled vsftpd and haproxy for quicker testing

--- srcs/5-kubernetes-dashboard/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-basedir=$(dirname "$0")
-
-kubectl apply -f $basedir/kubernetes-dashboard.yaml         \
-&& kubectl apply -f $basedir/cluster-admin.yaml             \
-&& kubectl apply -f $basedir/cluster-role-binding.yaml
-
-exit $?
