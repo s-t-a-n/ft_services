b454e416af5ab0b862dbfc7e0d7ea21514847c08
Stan Verschuuren <sverschu@student.codam.nl>
2020-10-15 16:36:21 +0200

    made wordpress run.sh more aggresive so it guarantees a wordpres install

--- srcs/6-wordpress/docker-srcs/run.sh
+++ srcs/6-wordpress/docker-srcs/run.sh
@@ -9,11 +9,12 @@ mkdir -p /run/nginx && mkdir -p /var/log/nginx && mkdir -p /data/http #|| exit 1
 
 # WORDPRESS
 WP_ROOT=/data/http/wordpress
-if [ ! -d $WP_ROOT ]; then
+if [ ! -d $WP_ROOT ] || [ ! -f $WP_ROOT/wp-config.php ]; then
+	rm -rf $WP_ROOT &>/dev/null || true
 	mkdir -p $WP_ROOT && chown -R nginx:nginx $WP_ROOT
-	su-exec nginx wp core download --path=$WP_ROOT #|| { rm -rf $WP_ROOT && exit 1 }
-	su-exec nginx wp config create --path=$WP_ROOT --dbname="$WP_DB_NAME" --dbuser="$WP_DB_USER" --dbpass="$WP_DB_PW" --dbhost="$WP_DB_HOST" #|| { rm -rf $WP_ROOT && exit 1 }
-	su-exec nginx wp core install --path=$WP_ROOT --admin_user="$WP_ADMIN" --admin_password="$WP_ADMIN_PW" --admin_email="$WP_ADMIN_MAIL" --skip-email #|| { rm -rf $WP_ROOT && exit 1 }
+	su-exec nginx wp core download --path=$WP_ROOT || { rm -rf $WP_ROOT && exit 1 }
+	su-exec nginx wp config create --path=$WP_ROOT --dbname="$WP_DB_NAME" --dbuser="$WP_DB_USER" --dbpass="$WP_DB_PW" --dbhost="$WP_DB_HOST" || { rm -rf $WP_ROOT && exit 1 }
+	su-exec nginx wp core install --path=$WP_ROOT --admin_user="$WP_ADMIN" --admin_password="$WP_ADMIN_PW" --admin_email="$WP_ADMIN_MAIL" --skip-email || { rm -rf $WP_ROOT && exit 1 }
 fi
 
 # PHP
