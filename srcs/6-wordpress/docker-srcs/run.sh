7bb7342c82e4015d48abfb8167173bd81ee2bc6e
s-t-a-n <stan@wopwop.cc>
2020-10-16 07:48:56 +0200

    guarantee that wordpress installs correctly or cleans up

--- srcs/6-wordpress/docker-srcs/run.sh
+++ srcs/6-wordpress/docker-srcs/run.sh
@@ -5,21 +5,21 @@ source /config/* #|| exit 1
 until nc -w1 -z $WP_DB_HOST 3306 >/dev/null 2>&1; do :; done
 
 # NGINX
-mkdir -p /run/nginx && mkdir -p /var/log/nginx && mkdir -p /data/http #|| exit 1
+mkdir -p /run/nginx && mkdir -p /var/log/nginx && mkdir -p /data/http
 
 # WORDPRESS
 WP_ROOT=/data/http/wordpress
 if [ ! -d $WP_ROOT ] || [ ! -f $WP_ROOT/wp-config.php ]; then
 	rm -rf $WP_ROOT 1>/dev/null 2>&1 || true
-	mkdir -p $WP_ROOT && chown -R nginx:nginx $WP_ROOT
-	su-exec nginx wp core download --path=$WP_ROOT #|| { rm -rf $WP_ROOT && exit 1 }
-	su-exec nginx wp config create --path=$WP_ROOT --dbname="$WP_DB_NAME" --dbuser="$WP_DB_USER" --dbpass="$WP_DB_PW" --dbhost="$WP_DB_HOST" #|| { rm -rf $WP_ROOT && exit 1 }
-	su-exec nginx wp core install --path=$WP_ROOT --admin_user="$WP_ADMIN" --admin_password="$WP_ADMIN_PW" --admin_email="$WP_ADMIN_MAIL" --skip-email #|| { rm -rf $WP_ROOT && exit 1 }
+	mkdir -p $WP_ROOT && chown -R nginx:nginx $WP_ROOT || { rm -rf $WP_ROOT && exit 1; }
+	su-exec nginx wp core download --path=$WP_ROOT || { rm -rf $WP_ROOT && exit 1; }
+	su-exec nginx wp config create --path=$WP_ROOT --dbname="$WP_DB_NAME" --dbuser="$WP_DB_USER" --dbpass="$WP_DB_PW" --dbhost="$WP_DB_HOST" || { rm -rf $WP_ROOT && exit 1; }
+	#su-exec nginx wp core install --path=$WP_ROOT --admin_user="$WP_ADMIN" --admin_password="$WP_ADMIN_PW" --admin_email="$WP_ADMIN_MAIL" --skip-email || { rm -rf $WP_ROOT && exit 1; }
 fi
 
 # PHP
 PHP_SUFFIX=$(echo $PHPFPM_VERSION | cut -f1 -d\.)
-mkdir -p /run/php-fpm$PHP_SUFFIX && mkdir -p /var/log/php$PHP_SUFFIX #|| exit 1
+mkdir -p /run/php-fpm$PHP_SUFFIX && mkdir -p /var/log/php$PHP_SUFFIX
 
 # UNIVERSAL
 rm -f /etc/motd
